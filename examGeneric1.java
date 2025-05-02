2378e69d50ed48b27332d0a70fe61a4c29528d18
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2025-05-02 11:31:46 +0100

    Add files via upload

--- examGeneric1.java
+++ examGeneric1.java
@@ -3,7 +3,10 @@ package MindGame;
 public class examGeneric1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "Dr. Joe Blog";
+		System.out.println(check(name,s ->s.startWith("Dr.")));
+		Integer i = -4;
+		System.out.println(check(i,n -> n>0));
 
 	}
 
